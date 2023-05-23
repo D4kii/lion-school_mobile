65d15915c3917b42151c6f1b6541bb31eb7ef579
DANIELA LINO DA SILVA OLIVEIRA <22282183@dsn127m1242269.cfp127aluno.local>
2023-05-23 17:00:44 -0300

    fazendo a tela de login

--- app/src/main/java/br/senai/sp/jandira/lionschool/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/MainActivity.kt
@@ -1,19 +1,19 @@
 package br.senai.sp.jandira.lionschool
 
+import android.R.color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,14 +45,36 @@ fun LoginScreen() {
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(10.dp)
+                .fillMaxSize()
                 .background(
                     color = Color(50, 60, 172)
                 )
 
         ) {
-            Text(text = "kidshf;sduhflkdsuhflkdsufhlsdufg")
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(500.dp)
+                    .shadow(
+                        40.dp,
+                        shape = RoundedCornerShape(0.dp),
+                        true,
+                        Color(52, 71, 176)
+                    ),
+                    color = Color(255, 255, 255),
+                shape = RoundedCornerShape( bottomEnd = 40.dp,
+                                            bottomStart = 40.dp),
+
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+
+                }
+            }
         }
     }
 }
